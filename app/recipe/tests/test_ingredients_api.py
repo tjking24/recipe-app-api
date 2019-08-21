ce3a5e82c305ea7b7f002747239d7bb89baa7b85
Tom King <thomasjking99@gmail.com#>
2019-08-21 21:15:31 +0100

    Refactor tests and ingredients

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -23,7 +23,7 @@ class PubicIngredientsApiTests(TestCase):
         """Test that login is required to access the endpoint"""
         res = self.client.get(INGREDIENTS_URL)
 
-        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateIngredientsApiTests(TestCase):
--- app/recipe/tests/test_tag_api.py
+++ app/recipe/tests/test_tag_api.py
@@ -23,7 +23,7 @@ class PublicTagsApiTests(TestCase):
         """Test that login is required for retriveing tags"""
         res = self.client.get(TAGS_URL)
 
-        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateTagsApiTests(TestCase):
